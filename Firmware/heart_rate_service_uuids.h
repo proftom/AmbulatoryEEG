8a08459e22fae5e06999e164491c30d45a066069
ProfTom <tm1810@ic.ac.uk>
2014-05-28 21:12:29 +0100

    removed some redudant HR defines

--- Firmware/heart_rate_service_uuids.h
+++ Firmware/heart_rate_service_uuids.h
@@ -39,28 +39,6 @@
 
 #define DEFAULT_ACQUISITION_RATE 			0xFF
 
-/* Heart Rate Sensor Location characteristic values */
-
-/* Body Sensor Location unknown */
-#define BODY_SENSOR_LOCATION_OTHER                    0x00
-/* Body Sensor Location chest */
-#define BODY_SENSOR_LOCATION_CHEST                    0x01
-/* Body Sensor Location wrist */
-#define BODY_SENSOR_LOCATION_WRIST                    0x02
-/* Body Sensor Location finger */
-#define BODY_SENSOR_LOCATION_FINGER                   0x03
-/* Body Sensor Location hand */
-#define BODY_SENSOR_LOCATION_HAND                     0x04
-/* Body Sensor Location ear lobe */
-#define BODY_SENSOR_LOCATION_EAR_LOBE                 0x05
-/* Body Sensor Location foot */
-#define BODY_SENSOR_LOCATION_FOOT                     0x06
-
-
-/* A user should change the following macro expansion to change the body 
- * location of HR sensor.
- */
-#define CURRENT_EEG_ACQUISITION_RATE_VALUE            BODY_SENSOR_LOCATION_CHEST
 
 
 #endif /* __HEART_RATE_SERVICE_UUID_H__ */

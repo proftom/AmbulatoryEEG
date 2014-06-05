ac74bf154c0a00fbbd528f0d57b101ee858757cc
ProfTom <tm1810@ic.ac.uk>
2014-06-05 11:52:05 +0100

    board  manufacture at school

---
+++ Application/Electroencephalograph/batteryService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Devices.Enumeration;
+using Windows.Devices.Enumeration.Pnp;
+
+using Windows.Storage.Streams;
+
+using System.Text;
+using System.Diagnostics;
+
+namespace Electroencephalograph
+{
+    class batteryService
+    {
+
+        public batteryService()
+        {
+            IsInitialised = true;
+        }
+
+        private async Task ConfigureServiceForNotificationsAsync()
+        {
+
+        }
+        public bool IsInitialised { get; private set; }
+        public static batteryService Instance
+        {
+            get { return instance; }
+        }
+        public GattDeviceService service { get; set; }
+
+        private static batteryService instance = new batteryService();
+        public GattDeviceService Service
+        {
+            get { return service; }
+        }
+
+        private async void batteryNotification(GattCharacteristic sender, GattValueChangedEventArgs args)
+        {
+            var data = new byte[args.CharacteristicValue.Length];
+            DataReader.FromBuffer(args.CharacteristicValue).ReadBytes(data);
+            System.Diagnostics.Debug.WriteLine(data[1]);
+        }
+    }
+}
